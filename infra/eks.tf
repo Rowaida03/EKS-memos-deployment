864223f7419244320912b5587320067a817111a1
Rowaida03 <abdirahmanrowaida@gmail.com>
2026-08-05 00:05:11 +0100

    fix: change instance type

--- infra/eks.tf
+++ infra/eks.tf
@@ -11,6 +11,7 @@ module "eks" {
   endpoint_public_access = true # your control planes api server is now accessible over https, the public internetaccess_entries. it now has a public endpoint 
   # restrict to your own ip 
   endpoint_private_access = true # the api server is also reachable from inside the VPC 
+  enable_cluster_creator_admin_permissions = true 
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
@@ -19,11 +20,11 @@ module "eks" {
   eks_managed_node_groups = { # defining the ec2 worker nodes that will run your pods
     workers = {
       disk_size      = 50 # ebs volume size - the local disk storage of each worker node
-      instance_types = ["t3a.large", "t3.large"]
+      instance_types = ["t2.micro", "t3.micro"]
 
       min_size     = 2
       max_size     = 6
-      desired_size = 2
+      desired_size = 3
 
     }
 
